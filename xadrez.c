1fd82f969584555fded34541c21122ff8098c4ad
Rian Moraes <rian.moraes2702@gmail.com>
2026-02-18 02:01:09 +0000

    tema 4: desafio nivel novato concluido

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,32 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int torre = 5, bispo = 1, rainha = 1;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    do
+    {
+        printf("o bispo moveu-se para: cima, direita\n");
+        bispo++;
+    } while (bispo <= 5);
+    
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    for (int i = 0; i < torre; i++)
+        {
+            printf("Torre moveu-se para a direita!\n");
+        }
+        
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    while (rainha <= 8)
+    {
+        printf("Rainha moveu-se para esquerda.\n");
+        rainha++;
+    }
+    
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
